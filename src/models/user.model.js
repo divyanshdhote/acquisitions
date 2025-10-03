6d9780271b1e44619c14ca841969d7f0c008c089
Divyansh <divyanshdhote77@gmail.com>
2025-10-03 16:05:43 +0530

    setup neon db with drizzle

---
+++ src/models/user.model.js
@@ -0,0 +1,11 @@
+import { serial, text, timestamp, pgTable, varchar } from 'drizzle-orm/pg-core';
+
+export const users = pgTable('users', {
+  id: serial('id').primaryKey(),
+  name: varchar('name', { length: 256 }).notNull(),
+  email: varchar('email', { length: 256 }).notNull().unique(),
+  password: text('password').notNull(),
+  role: text('role', { enum: ['user', 'admin'] }).default('user').notNull(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').defaultNow().notNull(),
+});
