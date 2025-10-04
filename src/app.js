7b38ee971f2603a784fdcb361872634c7e478d6e
Divyansh <divyanshdhote77@gmail.com>
2025-10-04 11:11:33 +0530

    add auth

--- src/app.js
+++ src/app.js
@@ -1,9 +1,50 @@
-import express from 'express';
+import express, { urlencoded } from 'express';
+import logger from './config/logger.js';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
+app.use(helmet());
+app.use(express.json());
+app.use(urlencoded({ extended: true }));
+
+app.use(
+  morgan('combined', {
+    steam: { write: message => logger.info(message.trim()) },
+  })
+);
+
+app.use(cors());
+app.use(cookieParser());
+
 app.get('/', (req, res) => {
+  logger.info('hello from acquisition');
   res.send('Hello from acquisitions');
+});
+
+app.get('/health', (req, res) => {
+  res.status(200).json({
+    status: 'OK',
+    timestamp: new Date().toISOString(),
+    uptime: process.uptime(),
+  });
+});
+
+app.get('/api', (req, res) => {
+  res.status(200).json({ message: 'Acquisitions API is running!' });
+});
+
+import authRoutes from '#routes/auth.routes.js';
+import usersRoutes from '#routes/users.routes.js';
+
+app.use('/api/auth', authRoutes);
+app.use('/api/users', usersRoutes);
+
+app.use((req, res) => {
+  res.status(404).json({ error: 'Route not found' });
 });
 
 export default app;
